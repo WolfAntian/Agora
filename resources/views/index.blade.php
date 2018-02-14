9e5ffee3b4231f7e8edda6119809e8fb39ea78e1
WolfAntian <a.wolfarth@hotmail.com>
2018-02-14 00:06:52 +0000

    updated top left on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/') }}">Boards</a>
+                        <a href="{{ url('/') }}">{{Auth::user()->name}}</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
